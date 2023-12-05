2ed114eee509a2fd9c65416caa2fa4c9a7977d79
Numair Shaikh <numairsh77@gmail.com>
2023-12-06 01:11:58 +0530

    [Numair] Publish on Github Pages.

--- src/components/Education/education.js
+++ src/components/Education/education.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './education.css';
 
-const isMobile = window.innerWidth <= 768;
 
 const Education = () => {
+
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className="container-fluid" id="education">
             <div className="row">
